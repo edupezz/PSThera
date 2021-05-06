ec78ba84390c0e3aa3fb20c9fab85c798ee6dd96
edupezz <edupezz@gmail.com>
2021-05-05 22:21:09 -0300

    Estrutura inicial e global styled

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { GlobalStyled } from './GlobalStyle';
+
+function App() {
+  return (
+    <>
+    <GlobalStyled />
+    <div className="App">
+      oi
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+body {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    width: 100%;
+}
+`
