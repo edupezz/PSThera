7e8a984740a2943ba934b924529c7585f79f16b4
edupezz <edupezz@gmail.com>
2021-05-07 04:06:28 -0300

    Alterações layout desktop view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,69 @@
 import { GlobalStyled } from './GlobalStyle';
+import styled from 'styled-components';
+
+import objects from './assets/objects';
+
+import {
+  ObjectsContainer,
+  ButtonsContainer,
+  LogoContainer,
+  SignUpButton,
+  LoginButton,
+  MenuLinks,
+  MainText,
+  MenuBar,
+  Logo
+} from './styles'
+
+const AppContainer = styled.main `
+
+  width: 100vw;
+  height: 100vh;
+`
+
+const AppContainerColorMask = styled.div `
+
+  width: 100vw;
+  height: 100vh;
+  background-color: #4621AD;
+  opacity: 0.83;
+  display: flex;
+  flex-direction: column;
+`
 
 function App() {
   return (
     <>
-    <GlobalStyled />
-    <div className="App">
-      oi
-    </div>
+      <GlobalStyled />
+        <AppContainer>
+          <AppContainerColorMask>
+            <LogoContainer>
+              <Logo>
+                <p>L</p>
+              </Logo>
+            </LogoContainer>
+            <MenuBar>
+              <MenuLinks>
+                <ul>
+                  <li><a href="#">HOME</a></li>
+                  <li><a href="#">ARTICLES</a></li>
+                  <li><a href="#">AUDIO</a></li>
+                </ul>
+              </MenuLinks>
+            </MenuBar>
+            <MainText>
+              <h1>Join us today</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </MainText>
+            <ObjectsContainer>
+              {objects}
+            </ObjectsContainer>
+            <ButtonsContainer>
+              <SignUpButton>Sign Up</SignUpButton>
+              <LoginButton>Login</LoginButton>
+            </ButtonsContainer>
+          </AppContainerColorMask>
+        </AppContainer>
     </>
   );
 }
---
+++ frontend/src/styles.js
@@ -0,0 +1,163 @@
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+
+export const LogoContainer = styled.div `
+
+    margin: 0 auto;
+    display: flex;
+    width: 16vw;
+    height: 11vh;
+    max-width: 1    6vw;
+    max-height: 12vh;
+    background-color: #fff;
+    -webkit-border-bottom-right-radius: 45px;
+    -webkit-border-bottom-left-radius: 45px;
+    -moz-border-radius-bottomright: 45px;
+    -moz-border-radius-bottomleft: 45px;
+    border-bottom-right-radius: 45px;
+    border-bottom-left-radius: 45px;
+`
+
+export const Logo = styled.div `
+
+    margin: auto;
+    width: 3vw;
+    height: 6vh;
+    background-color: #5F41B6;
+    -webkit-border-radius: 11px;
+    -moz-border-radius: 11px;
+    border-radius: 11px;
+    display: flex;
+    justify-content: center;
+
+    p{
+        font-family: 'Montserrat', sans-serif;
+        font-weight: 800;
+        color: #FFEE00;
+        text-shadow: #FFEE00 0px 0px;
+        font-size: 150%;
+        text-align: center;
+    }
+
+`
+export const MenuBar = styled.div `
+
+    width: 100vw;
+    height: 8vh;
+    display: flex;
+    flex-direction: row;
+`
+
+export const MenuLinks = styled.nav`
+
+    color: #fff;
+
+    ul{
+        margin-left: 9vw;
+        margin-top: 4vh;
+    }
+
+
+    li{
+        text-decoration: none;
+        list-style-type: none;
+        display: inline;
+        margin-left: 3.5vw;
+        font-family: 'Montserrat', sans-serif;
+        font-weight: 300;
+        font-size: 70%;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+`
+
+export const MainText = styled.div `
+
+    display: flex;
+    flex-direction: column;
+    min-height: 26vh;
+    justify-content: center;
+    padding: 2vh;
+
+    h1{
+        margin: 0 auto;
+        text-align: center;
+        font-size: 200%;
+        font-weight: bold;
+        margin-top: 8vh;
+    }
+
+    p{
+        margin: 0 auto;
+        text-align: center;
+        max-width: 40%;
+        padding-top: 1.5vh;
+        font-size: 75%;
+    }
+`
+
+export const ObjectsContainer = styled.div `
+
+    width: 65vw;
+    margin: 0 auto;
+
+    svg{
+        width: 90%;
+    }
+`
+
+export const ButtonsContainer = styled.div`
+
+    width: 40vw;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-self: center;
+    margin-top: 2vh;
+`
+
+export const SignUpButton = styled(Button) `
+
+    &&{
+        background-color: transparent;
+        color: #fff;
+        width: 14vw;
+        border: thin solid white;
+        -webkit-border-radius: 45px;
+        -moz-border-radius: 45px;
+        border-radius: 45px;
+    }
+
+    &&.MuiButton-root{
+        text-transform: capitalize;
+        line-height: 1.5;
+        font-family: 'Montserrat', sans-serif;
+        }
+`
+
+export const LoginButton = styled(Button)`
+
+    &&{
+        background-color: white;
+        color: #4621AD;
+        width: 14vw;
+        border: thin solid white;
+        -webkit-border-radius: 45px;
+        -moz-border-radius: 45px;
+        border-radius: 45px;
+    }
+
+    &&.MuiButton-root{
+        text-transform: capitalize;
+        font-family: 'Montserrat', sans-serif;
+        line-height: 1.5;
+    }
+
+    &&:focus{
+        background-color: white;
+        color: #4621AD;
+    }
+
+`
